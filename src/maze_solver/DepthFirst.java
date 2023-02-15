b6d45caa59cdf0aaf04d9e8e767a71a21b5dee0c
Roshan Kumar <roshangupta00112@gmail.com>
2023-02-15 21:55:06 +0530

    Dynamic Maze_Solver

--- src/maze_solver/DepthFirst.java
+++ src/maze_solver/DepthFirst.java
@@ -8,53 +8,58 @@ import java.util.List;
 
 /**
  *
- * @author ROSHAN GUPTA
+ * @author Meteor
  */
 public class DepthFirst {
-    public static boolean searchPath(int[][]maze, int x, int y, List<Integer> path){
-      if(maze[x][y]==13){ 
-          path.add(x);
-          path.add(y);
-          return true;
-      }
-      if(maze[x][y]==0){
-          maze[x][y]=2;
-          
-          // Upward Direction
-          int dx=-1;
-          int dy=0;
-          if(searchPath(maze, x+dx, y+dy, path)){
-              path.add(x);
-              path.add(y);
-              return true;
-          }
-          // Downward Direction
-          dx=1;
-          dy=0;
-          if(searchPath(maze, x+dx, y+dy, path)){
-              path.add(x);
-              path.add(y);
-              return true;
-          }
-          //Left Direction
-          dx=0;
-          dy=-1;
-          if(searchPath(maze, x+dx, y+dy, path)){
-              path.add(x);
-              path.add(y);
-              return true;
-          }
-          // Right Direction
-          dx=0;
-          dy=1;
-          if(searchPath(maze, x+dx, y+dy, path)){
-              path.add(x);
-              path.add(y);
-              return true;
-          }
-          
-      }
-      
-      return false;
-    }
+    //in DFS we mark the visited node as vistsed like yes i have covered the node
+    //for priniting a path a line so need to add x,y need arraylist
+    public static boolean searchpath(int[][] maze, int x, int y, List<Integer> path){
+        if(maze[y][x]==9){
+            // 9 is the end(finish) value add those cordinates
+            path.add(x);
+            path.add(y);
+            return true;       
+        }
+        //if ==0 moveR,L,T,B also need to check blocker
+        if(maze[y][x]==0){
+            // dont change its value once its visited
+            maze[y][x]=2;// 2 is kepts as condition to mark as visited
+            // x in down(row) and y in rightside(colums)
+            //upwards direction
+            int dx=-1;
+            int dy=0;
+            //recusion call to maze n change cordinates
+            if (searchpath(maze, x+dx,y+dy,path)){
+                path.add(x);
+                path.add(y);
+                return true;
+            }
+            //downwards
+            dx=1;
+            dy=0;
+            if (searchpath(maze, x+dx,y+dy,path)){
+                path.add(x);
+                path.add(y);
+                return true;
+            }
+            //left
+            dx=0;
+            dy=-1;
+            if (searchpath(maze, x+dx,y+dy,path)){
+                path.add(x);
+                path.add(y);
+                return true;
+            }
+            //right
+            dx=0;
+            dy=1;
+            if (searchpath(maze, x+dx,y+dy,path)){
+                path.add(x);
+                path.add(y);
+                return true;
+            } 
+        }
+        //if value is not 9 and 0 then it will be 1 so return false
+        return false;
+    } 
 }
--- src/maze_solver/NewClass.java
+++ src/maze_solver/NewClass.java
@@ -6,79 +6,132 @@ package maze_solver;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  *
- * @author ROSHAN GUPTA
+ * @author Meteor
  */
 public class NewClass extends JFrame{
-    private int[][] maze=
-   // {{0,1,1,1,0,0,0,0,0,0,0,1,1},
-     {{1,1,1,1,1,1,1,1,1,1,1,1,1},
-     {1,0,0,0,0,0,0,1,1,0,0,1,1},
-     {1,1,0,0,1,1,1,1,1,0,1,0,1},
-     {1,1,1,0,0,0,0,0,1,0,0,0,1},
-     {1,1,0,1,0,0,1,0,1,0,1,1,1},
-     {1,1,0,0,0,0,1,0,0,0,1,1,1},
-     {1,1,1,0,0,0,1,1,1,0,0,0,1},
-     {1,0,0,0,1,1,0,1,0,1,1,1,1},
-     {1,1,1,0,1,1,0,1,0,0,0,1,1},
-     {1,0,1,0,1,1,1,1,0,1,0,1,1},
-     {1,1,1,0,0,0,0,0,0,1,0,0,1},
-     {1,0,1,1,1,1,1,1,1,0,1,13,1},
-     {1,1,1,1,1,1,1,1,1,1,1,1,1},
+    //relate dto Gui,2d array everything will b here
+    public int [][] maze=
+    {     {1,1,1,1,1,1,1,1,1,1},
+          {1,0,1,0,0,1,0,0,0,1},
+          {1,0,1,1,0,1,0,1,0,1},
+          {1,0,1,0,0,0,0,1,0,1},
+          {1,0,0,0,1,0,1,1,1,1},
+          {1,1,0,1,1,0,0,0,0,1},
+          {1,0,0,0,0,1,0,1,0,1},
+          {1,0,1,0,1,9,1,1,0,1},
+          {1,0,0,0,0,0,0,0,0,1},
+          {1,1,1,1,1,1,1,1,1,1}
+          
     };
     public List<Integer> path=new ArrayList<>();
-    
+     public int pathIndex;
+     //GUI below
     public NewClass(){
-        setTitle("Maze Solver");
+        setTitle("Maze Game Solver");
         setSize(640,480);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //DepthFirst.searchPath(maze, 0, 0, path);
-        DepthFirst.searchPath(maze, 1, 1, path);
+        //call our dfs class
+        DepthFirst.searchpath(maze,1,1,path);
         System.out.println(path);
+        pathIndex = path.size() - 2;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                update();
+                repaint();
+            }
+        }, 100, 500);        
+        
+    }
+    public void update() {
+        pathIndex -= 2;
+        if (pathIndex < 0) {
+            pathIndex = 0;
+        }
     }
     
     @Override
-    public void paint(Graphics g){
-        //for centralisation
-        g.translate(50,50);
+    public void paint(Graphics g) {
+        //components should render properly so we use super class
         super.paint(g);
-        for(int i=0; i<maze.length; i++){
-            for(int j=0; j<maze[0].length; j++){
+        g.translate(50, 50);// make it centralized
+        
+        for (int row=0;row<maze.length;row++) {
+            for (int col=0;col<maze[0].length;col++) {
                 Color color;
-                switch(maze[i][j]){
-                    case 1:color=Color.BLACK;
-                    break;
-                    case 13: color=Color.RED;
-                    break;
-                    default: color=Color.WHITE;
-                    break;
+                switch(maze[row][col]) {
+                    case 1 : color=Color.BLACK; break;//blocker or wall
+                    case 9 : color=Color.BLUE; break;// finish line
+                    default : color=Color.WHITE; break;
+                    
                 }
+                //setting color to maze
                 g.setColor(color);
-                //to fill the each grid
-                g.fillRect(30*i, 30*j, 30, 30);
-                g.setColor(Color.RED);
-                // for drawing boundary of each grid
-                g.drawRect(30*i, 30*j, 30, 30);
+                g.fillRect(30*col,30*row,30,30);//outline color
+                g.setColor(Color.BLUE);
+                g.drawRect(30*col,30*row,30,30);
+              
             }
         }
-        
-        for(int i=0; i<path.size(); i+=2){
-            int pathx=path.get(i);
-            int pathy=path.get(i+1);
-            System.out.println("X coordinates "+ pathx);
-            System.out.println("Y coordinates "+ pathy);
-            g.setColor(Color.BLUE);
-            g.fillRect(30*pathx,  30*pathy, 30, 30);
+        // draw the path line
+        for(int p=0;p<path.size();p+=2){
+            //y is it +2 bcoz we at xindex at +1 we will be at y index 
+            int pathx=path.get(p);
+            int pathy=path.get(p+1);
+            System.out.println("path x coordinate"+pathx);
+            System.out.println("path y coordinate"+pathy);
+            g.setColor(Color.ORANGE);
+            g.fillRect(pathx*30, pathy*30, 30, 30);
+            g.setColor(Color.CYAN);
+            g.drawRect(30*pathx,30*pathy,30,30);
+            
+            
         }
+//        draw the ball
+        int pathX = path.get(pathIndex);
+        int pathY = path.get(pathIndex + 1);
+        g.setColor(Color.RED);
+        g.fillOval(pathX * 30, pathY * 30, 30, 30);
+    }
+    @Override
+    protected void processKeyEvent(KeyEvent ke) {
+        if (ke.getID() != KeyEvent.KEY_PRESSED) {
+            return;
+        }
+        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+            pathIndex -= 2;
+            if (pathIndex < 0) {
+                pathIndex = 0;
+            }
+        }
+        else if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+            pathIndex += 2;
+            if (pathIndex > path.size() - 2) {
+                pathIndex = path.size() - 2;
+            }
+        }
+        repaint(); 
     }
     
     public static void main(String[] args){
-        NewClass view=new NewClass();
-        view.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                NewClass view =new NewClass();
+                view.setVisible(true);
+            }         
+        }
+        );    
     }
 }
